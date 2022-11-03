7f69978135dade8e8e87d53297aafbfad18d5e31
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2022-11-03 21:16:45 +0700

    Add stop when it comes to the edge of image

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 currSlide = 0;
 
+
 const slides = document.querySelectorAll(".slide");
 slides.forEach((slide, index) => {
   slide.style.transform = `translateX(${index * 100}%)`
 })
 
+maxSlide = slides.length - 1;
+
 nextSlide = document.querySelector(".next");
 nextSlide.addEventListener("click", () => {
-  currSlide++;
+  if (currSlide === maxSlide) {
+    currSlide = 0;
+  } else {
+    currSlide++;
+  }
+
   slides.forEach((slide, index) => {
     slide.style.transform = `translateX(${100 * (index - currSlide)}%)`;
     console.log(index, currSlide);
@@ -16,7 +24,11 @@ nextSlide.addEventListener("click", () => {
 
 backSlide = document.querySelector(".back");
 backSlide.addEventListener("click", () => {
-  currSlide--;
+  if (currSlide === maxSlide) {
+    currSlide = 0;
+  } else {
+    currSlide--;
+  }
   slides.forEach((slide, index) => {
     slide.style.transform = `translateX(${100 * (index - currSlide)}%)`;
   })
